687b974629761e541828643245c543f6c4d8a91c
ishtiaque-fahim <ishtiaquefaahim@gmail.com>
2026-07-31 12:52:01 +0600

    fix: set authDomain to Vercel domain

--- tourhobe-frontend/src/firebase.js
+++ tourhobe-frontend/src/firebase.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, GithubAuthProvider, connectAuthEmulator } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDH19RJUeeFxa3Y_j2chSkcktAhVicBJcc",
-    authDomain: "tourhobe-356a5.firebaseapp.com",
+    authDomain: "tour-hobe-orcin.vercel.app",
     projectId: "tourhobe-356a5",
     storageBucket: "tourhobe-356a5.firebasestorage.app",
     messagingSenderId: "316109253708",
