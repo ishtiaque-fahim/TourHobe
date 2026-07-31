dfec435ed42eb08837b1bf548234d4ed317f9402
ishtiaque-fahim <ishtiaquefaahim@gmail.com>
2026-07-31 22:37:02 +0600

    fix: clean App.jsx remove unused imports

--- tourhobe-frontend/src/App.jsx
+++ tourhobe-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -9,21 +9,15 @@ import ResortDetail from "./pages/ResortDetail";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import PrivateRoute from "./components/PrivateRoute";
-import { useAuth } from "./context/AuthContext";
-import AuthCallback from "./pages/AuthCallback";
-import GoogleCallback from "./pages/GoogleCallback";
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="google-callback" element={<GoogleCallback />} />
         <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
-          <Route index element={<Home />} />
-          <Route path="auth/callback" element={<AuthCallback />} />
           <Route path="resorts" element={<Resorts />} />
           <Route path="resorts/:id" element={<ResortDetail />} />
           <Route path="about" element={<About />} />
