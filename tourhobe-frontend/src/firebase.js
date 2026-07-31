1895b079eb3691f57c71febd012cdd9f6a81d2f8
ishtiaque-fahim <ishtiaquefaahim@gmail.com>
2026-07-31 12:55:40 +0600

    fix: revert authDomain to Firebase domain

--- tourhobe-frontend/src/firebase.js
+++ tourhobe-frontend/src/firebase.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, GithubAuthProvider, connectAuthEmulator } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDH19RJUeeFxa3Y_j2chSkcktAhVicBJcc",
-    authDomain: "tour-hobe-orcin.vercel.app",
+    authDomain: "tourhobe-356a5.firebaseapp.com",
     projectId: "tourhobe-356a5",
     storageBucket: "tourhobe-356a5.firebasestorage.app",
     messagingSenderId: "316109253708",
